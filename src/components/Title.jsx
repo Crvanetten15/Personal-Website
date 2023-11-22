4006e4b05426c3784a1e29a0997219231b9ca831
Connor Van Etten <87097190+Crvanetten15@users.noreply.github.com>
2023-11-22 11:40:25 -0500

    Init home pages

---
+++ src/components/Title.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+import '../styles/Title.css'; // Import the CSS file
+
+const Title = () => {
+  return (
+    <>
+    <p id='opening-words'>
+      Hi I&apos;m
+      <h1>
+        <span id = "fname-styling">Connor</span> Van Etten
+      </h1>
+      <span id='under-text'>
+      As a dedicated software engineer, I thrive on transforming innovative ideas into functional and efficient software solutions, continuously seeking opportunities to enhance user experiences and solve complex problems.
+      </span>
+    </p>
+    </>
+  );
+};
+
+export default Title;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
-// Header.js
-
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
+
 import '../styles/Home.css'; // Import the CSS file
+import Title from '../components/Title';
 
 const Home = () => {
   return (
-    <a>
+    <>
+    <div className='back-image'>
+      <div id='left-panel'>
+        <Title />
+      </div>
+      <div>
 
-    </a>
+      </div>
+    </div>
+    </>
   );
 };
 
